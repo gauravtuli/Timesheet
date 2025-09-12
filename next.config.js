8cdac1285a09427a88b66faeda017eeff1349d1d
gauravtuli <47097043+gauravtuli@users.noreply.github.com>
2025-09-12 10:32:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   experimental: { serverActions: { bodySizeLimit: '2mb' } },
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
 }
+
 module.exports = nextConfig
